81c36276dbaac9036a363eaf841dd00c883ac67b
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-26 23:01:14 +0200

    eerste javascript

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("Hello Winc Academy");
+let ownName = "Sharite";
+console.log(ownName);
+let number1 = 4 + 4;
+console.log(number1);
+let number2 = 4 - 4;
+console.log(number2);
+let number3 = 8 * 3;
+console.log(number3);
+let number4 = 27 / 9;
+console.log(number4);
+let number5 = 2 % 100;
+console.log(number5);
+let age = 27;
+console.log(typeof 27);
+let age2 = "zevenentwintig";
+console.log(typeof zevenentwintig);

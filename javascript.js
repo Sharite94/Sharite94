a13b8946b65e3bcb92e000be1e896d4d1dddc221
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-28 22:29:37 +0200

    three ways to write them

--- javascript.js
+++ javascript.js
@@ -1,12 +1,26 @@
-const paintwalls = function (item) {
-  console.log("The wall has been painted " + item);
+function value(number) {
+  const outcome1 = number * number,
+  const outcome2 = outcome1 + outcome1,
+  const outcome3 = outcome2 * outcome2;
+  return outcome3;
 };
 
-const paintingwalls = function (item1, item2) {
-  console.log("The " + item1, "wall has been painted " + item2);
+
+const value2 = function(number) {
+  const outcome1 = number * number,
+  const outcome2 = outcome1 + outcome1,
+  const outcome3 = outcome2 * outcome2;
+  return outcome3;
 };
 
-paintwalls("green");
-paintwalls("blue");
 
-paintingwalls("South-east", "Yellow");
+
+const value3 = () => {
+  const outcome1 = number * number,
+  const outcome2 = outcome1 + outcome1,
+  const outcome3 = outcome2 * outcome2;
+  return outcome3;
+  console.log(value3(5));
+};
+
+value(5);

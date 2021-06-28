02eed00d1473168d17c70bd81b9110bb89e7209d
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-28 23:13:54 +0200

    functions calling functions

--- javascript.js
+++ javascript.js
@@ -1,26 +1,23 @@
-function value(number) {
-  const outcome1 = number * number,
-  const outcome2 = outcome1 + outcome1,
-  const outcome3 = outcome2 * outcome2;
-  return outcome3;
+const adult = function (age) {
+  if (age >= 18) {
+    return true;
+  }
 };
 
-
-const value2 = function(number) {
-  const outcome1 = number * number,
-  const outcome2 = outcome1 + outcome1,
-  const outcome3 = outcome2 * outcome2;
-  return outcome3;
+const sayHi = function () {
+  if (adult) {
+    return "Hello there";
+  } else {
+    return "Hey kiddo";
+  }
 };
 
+adult(20);
+console.log(adult(20));
+console.log(sayHi(20));
 
-
-const value3 = () => {
-  const outcome1 = number * number,
-  const outcome2 = outcome1 + outcome1,
-  const outcome3 = outcome2 * outcome2;
-  return outcome3;
-  console.log(value3(5));
+const totalprice = function (baseprice, VAT) {
+  return baseprice + VAT;
 };
 
-value(5);
+const price = (baseprice * 1, 21);

4a71993c3a1136fe91928697717844c227bc4ff8
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-28 20:58:16 +0200

    Function arguments

---
+++ javascript.js
@@ -0,0 +1,12 @@
+const paintwalls = function (item) {
+  console.log("The wall has been painted " + item);
+};
+
+const paintingwalls = function (item1, item2) {
+  console.log("The " + item1, "wall has been painted " + item2);
+};
+
+paintwalls("green");
+paintwalls("blue");
+
+paintingwalls("South-east", "Yellow");

cd082b424a8dde81ee86d3e0ae4871e490ab29f0
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-28 14:37:47 +0200

    function basics

---
+++ Javascript.js
@@ -0,0 +1,19 @@
+const makePizza = function(){
+    console.log("bodem maken");
+    console.log("spaghettie saus");
+    console.log("ingredienten erop");
+    
+};
+
+const makeSushi = function () {
+    console.log("rijst koken");
+    console.log("zalm snijden");
+    console.log("groentes toevoegen");
+}
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+makePizza();
+makePizza();

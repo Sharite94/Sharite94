6a95bacfeee9058687aa6653b1efe962406c4a9e
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-06-30 23:03:08 +0200

    opdracht20

---
+++ whileloop.js
@@ -0,0 +1,26 @@
+const colors = ["yellow", "blue", "red", "orange"];
+let i = 0;
+
+while (colors[i]) {
+    console.log(${i});
+  i++;
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(`This is for iteration number ${i}`);
+  }
+
+
+
+
+// while
+let w = 0;
+while (w < 5) {
+  console.log(`This is while iteration number ${w}`);
+  w++;
+}
+
+// for
+for (let i = 0; i < 5; i++) {
+  console.log(`This is for iteration number ${i}`);
+}

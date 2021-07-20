0ffae4ec07a27b2a6b97fa67a5cf189b33ecad6c
Sharite94 <83730239+Sharite94@users.noreply.github.com>
2021-07-20 14:47:34 +0200

    Gebruik eventhandlers

--- script.js
+++ script.js
@@ -1,36 +1,10 @@
-let person = {
-  name: "Sharite",
-  age: 27,
-};
+const mybutton = document.getElementById("mybutton");
 
-let evaluations = [7, 10, 9];
+mybutton.addEventListener("click", function () { alert("button clicked"); });
 
-let selectedColors = ["green", "blue", "red"];
+const changebackground = document.querySelector('.blue-background');
 
-selectedColors[3] = "yellow";
+changebackground.addEventListener("click",function(){
+	document.body.style.backgroundColor = "red";
+	});
 
-selectedColors[4] = "5";
-
-selectedColors[5] = { greeting: "hi ik ben een object" };
-
-//Not Notation
-person.name = "Sharite";
-console.log(person.name);
-
-//Brackert Notation
-let selection = "age";
-person[selection] = "27";
-
-console.log(person.age);
-
-console.log(evaluations);
-
-console.log(selectedColors);
-
-console.log(selectedColors.length);
-
-console.log(selectedColors[0]);
-
-console.log(selectedColors[2]);
-
-console.log(selectedColors[5]);
